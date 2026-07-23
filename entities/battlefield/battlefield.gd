85b2a8583bfd1b9f1a0cb68281876cacd86dc1d6
= <=>
2026-07-24 09:30:22 +1000

    feat: countdown

---
+++ entities/battlefield/battlefield.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var count_down_label: Control = $CountDownLabel/CountDownLabel
+@onready var count_down_timer: Timer = $CountDownTimer
+
+var previous_time: int
+
+func _ready():
+	previous_time = int( count_down_timer.time_left )
+	print(previous_time)
+
+func _process(_delta):
+	count_down_label.text = "%04.1f" % count_down_timer.time_left
+	var ciel_time_left = ceil(count_down_timer.time_left)
+	if ciel_time_left < previous_time:
+		previous_time = ciel_time_left
+		GameManager.second_tick.emit()
--- entities/utilities/game_manager/game_manager.gd
+++ entities/utilities/game_manager/game_manager.gd
@@ -1,1 +1,3 @@
 extends Node2D
+
+signal second_tick

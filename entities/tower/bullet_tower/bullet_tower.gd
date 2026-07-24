0ef598c38c65080c7e24921b362fafe9f622f184
= <=>
2026-07-24 18:22:30 +1000

    feat: updated tower sprite

--- entities/tower/bullet_tower/bullet_tower.gd
+++ entities/tower/bullet_tower/bullet_tower.gd
@@ -5,4 +5,5 @@ const BULLET = preload("res://entities/tower/bullet_tower/bullet/bullet.tscn")
 func _activate():
 	var bullet = BULLET.instantiate()
 	bullet.position = position
+	bullet.position.y += 25
 	get_parent().add_child(bullet)

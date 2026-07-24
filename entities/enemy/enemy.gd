ecdf1974b5174eb7023b2e7fe8271090706a3449
= <=>
2026-07-24 13:00:58 +1000

    feat: enemy damage

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -2,6 +2,8 @@ extends Area2D
 class_name Enemy
 
 @export var speed: int = 100
+@export var health: int = 100
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _process(delta):
 	_process_movement(delta)
@@ -30,3 +32,14 @@ func _process_knockback(delta):
 
 	if knockback_progress >= 1:
 		knockback = false
+
+#s DAMAGE ##
+func hit(damage: int):
+	var tween = get_tree().create_tween()
+	tween.tween_method(SetShader_BlinkIntensity, .9, 0.0, 0.5)
+	health -= damage
+	if health <= 0:
+		queue_free()
+
+func SetShader_BlinkIntensity(newValue: float):
+	sprite.material.set_shader_parameter("blink_intensity", newValue)
--- entities/tower/bullet_tower/bullet/bullet.gd
+++ entities/tower/bullet_tower/bullet/bullet.gd
@@ -1,13 +1,13 @@
 extends Area2D
 
 var speed: float = 200
+@export var damage: int = 50
 
 func _process(delta):
 	position += Vector2(0, speed * delta)
 
-
 func _on_area_entered(area:Area2D):
-	print("Bullet hit")
 	if area is Enemy:
 		area.knock_back()
+		area.hit(damage)
 		queue_free()

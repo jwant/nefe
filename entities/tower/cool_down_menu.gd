a121341da9b939697531d1d7151df56025fde6a7
= <=>
2026-07-26 09:00:12 +1000

    feat: cooldown settings

---
+++ entities/tower/cool_down_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _on_mouse_exited():
+	visible = false
+
--- entities/tower/tower.gd
+++ entities/tower/tower.gd
@@ -6,6 +6,8 @@ class_name Tower
 @export var cost: int = 25
 var cooldown: int = starting_cooldown
 @onready var cooldown_label: Label = $Stats/CoolDown
+@onready var cooldown_menu: Control = $CoolDownMenu
+@onready var area: Area2D = $Area2D
 
 func _ready():
 	if constructor:
@@ -14,12 +16,27 @@ func _ready():
 		GameManager.second_tick.connect(self._on_second_tick)
 		cooldown_label.text = str(cooldown)
 
+	if constructor:
+		return
+
+	for i in ["1","2","3"]:
+		var button = cooldown_menu.find_child(i)
+		if button is Button:
+			button.pressed.connect(func():
+				starting_cooldown = int(i)
+			)
+	area.input_event.connect(_tower_input)
+
+func _tower_input(_viewport, event, _shape_idx):
+	if event.is_action_pressed("mouse_left"):
+		cooldown_menu.visible = true
+	
 func _on_second_tick():
 	cooldown -= 1
 	if cooldown > 0:
 		cooldown_label.text = str(cooldown)
 	if cooldown == 0:
-		cooldown_label.text = "*"
+		cooldown_label.text = "0"
 		_activate()
 	if cooldown < 0:
 		cooldown = starting_cooldown
@@ -38,10 +55,10 @@ func on_grid_placed(grid: DraggableGrid):
 			new_tower.starting_cooldown = starting_cooldown
 			new_tower.cost = cost
 			grid.get_parent().add_child(new_tower)
-	else:
+	elif grid.global_position != global_position:
 		global_position = grid.global_position
+	else:
+		cooldown_menu.visible = true
 		
-
 func _random_projectile_direction():
 	return Vector2.DOWN.rotated(randf_range(-PI/8, PI/8))
-	
